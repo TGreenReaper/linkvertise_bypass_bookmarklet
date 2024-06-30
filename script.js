58b9cd20bcfa133a87a1fcff08550897035cd7dc
TGreenReaper <140297612+TGreenReaper@users.noreply.github.com>
2024-06-30 15:09:05 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var option = prompt("1. Automatic\n2. URL");
+
+if(option) {
+  if(option == '1'){
+     window.open(`https://bypass.city/bypass?bypass=${location.href}`)
+  } else if(option == "0") {
+    let p = prompt("URL: ");
+    if(p.includes("https://")){
+      window.open(`https://bypass.city/bypass?bypass=${p}`)
+    } else {
+      alert("Re run it but use HTTPS")
+    }
+  } else {
+    alert("Re run it but only include numbers")
+  }
+}
