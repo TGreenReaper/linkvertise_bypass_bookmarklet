f3e5f5cdc65388c8073b92f3d9e2596bd720f2fc
TGreenReaper <140297612+TGreenReaper@users.noreply.github.com>
2024-06-30 15:31:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ if(option) {
     } else {
       alert("Re run it but use HTTPS")
     }
-  } else {
-    alert("Re run it but only include numbers")
   } else if(option == null) {
     alert("Re Run it but put a fucking option")
-}
+} else if(option == ""){
+    alert("Do not leave it blank stupid"); document.body.style.backgroundImage = "url('https://wallpapercave.com/wp/HpE4ox4.jpg')"; document.body.style.backgroundRepeat = "no-repeat";document.body.style.backgroundSize = "auto";
+  }
+  else {
+    alert("Re run it but only include numbers")
+  } 
